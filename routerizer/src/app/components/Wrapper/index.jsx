065d52117de0f2f1be99a6b134eac4727d0b510f
Luiz Provenzi <86327853+LuizProvenzi@users.noreply.github.com>
2023-10-09 20:30:28 -0300

    First commit

---
+++ routerizer/src/app/components/Wrapper/index.jsx
@@ -0,0 +1,12 @@
+import { Wrapper } from "./styles";
+
+export default function Wrapper(props) {
+    const { width, height } = props
+
+    return (
+      <Wrapper width={width} height={height}>
+        Teste wrapper
+      </Wrapper>
+    )
+  }
+  
---
+++ routerizer/src/app/loading.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main>
+        <div>loading</div>
+      </main>
+    )
+  }
+  
