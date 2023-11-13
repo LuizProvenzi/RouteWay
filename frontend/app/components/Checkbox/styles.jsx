4b150ad143d1c6fdecae3f63a449574072f0266d
Luiz Provenzi <86327853+LuizProvenzi@users.noreply.github.com>
2023-11-13 14:20:19 -0300

    Delete frontend directory

--- frontend/app/components/Checkbox/styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { margins } from '@/app/assets/styles.jsx'
-
-export const StyledCheckbox = styled.input`
-  height: ${(props) => props.size}px;
-  width: ${(props) => props.size}px;
-  margin: ${(props) => margins[props.margin]};
-`
